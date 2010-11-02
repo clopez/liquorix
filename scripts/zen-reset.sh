492404bb0919aab420511fe9a17b9faa4f4f729b
Dominic Duklas <dodo1122@zen-kernel.org>
2010-11-02 23:48:50 +0100

    fix damn vim spacing

--- scripts/zen-reset.sh
+++ scripts/zen-reset.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Resetting the repository..."
-git add .; git reset --hard; git branch -M master master-old; git checkout -b
-master origin/master; git branch -D master-old
+git add .; git reset --hard; git branch -M master master-old; git checkout -b master origin/master; git branch -D master-old
 
 echo "Done!"
 
