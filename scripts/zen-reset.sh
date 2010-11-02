3c25c5be0b5f9426512e9bb3d2eca2bf351253fe
Dominic Duklas <dodo1122@zen-kernel.org>
2010-11-02 23:45:43 +0100

    add zen-reset script

---
+++ scripts/zen-reset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Resetting the repository..."
+git add .; git reset --hard; git branch -M master master-old; git checkout -b
+master origin/master; git branch -D master-old
+
+echo "Done!"
+
